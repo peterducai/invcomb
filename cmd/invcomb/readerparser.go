8b665a209cb9bda29e3f51289d920901e84007d2
pducai <pducai@dhcp-28-174.brq.redhat.com>
2019-10-30 13:50:58 +0100

    reading groups

--- cmd/invcomb/readerparser.go
+++ cmd/invcomb/readerparser.go
@@ -1,10 +1,32 @@
 package invcomb
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 //ReadFile to read inventories
-func ReadFile(string) {
-	fmt.Println("reading file")
+func ReadFile(invfile string) {
+
+	file, err := os.Open(invfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "[") {
+			fmt.Println(line)
+		}
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
--- invcomb.go
+++ invcomb.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -11,11 +12,18 @@ import (
 func main() {
 	fmt.Println("inventory combinator")
 
+	invInput := flag.String("input", "define_input_inventories", "inventories to combine")
+	invOutput := flag.String("output", "examples/generated_inventory.yml", "inventory to create from input inventories")
+	//boolPtr := flag.Bool("fork", false, "a bool")
+	flag.Parse()
+
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(dir)
 
-	invcomb.ReadFile("skdjfls")
+	invcomb.ReadFile(*invInput)
+
+	fmt.Printf("\nwill create %s in %s\n", *invOutput, dir)
 }
