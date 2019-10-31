6eb26de811f3cfa208cbd803f44d3c95741b366c
pducai <pducai@localhost.localdomain>
2019-10-31 17:04:02 +0100

    tmp save

--- cmd/invcomb/inventory_tree.go
+++ cmd/invcomb/inventory_tree.go
@@ -8,6 +8,43 @@ import (
 	"strings"
 )
 
+//jumper ansible_port=5555 ansible_host=192.0.2.50
+//[databases]
+//db-[a:f].example.com
+
+//[targets]
+//
+//localhost              ansible_connection=local
+//other1.example.com     ansible_connection=ssh        ansible_user=mpdehaan
+//other2.example.com     ansible_connection=ssh        ansible_user=mdehaan
+
+// [atlanta]
+// host1
+// host2
+
+// [atlanta:vars]
+// ntp_server=ntp.atlanta.example.com
+// proxy=proxy.atlanta.example.com
+
+// [southeast:vars]
+// some_server=foo.southeast.example.com
+// halon_system_timeout=30
+// self_destruct_countdown=60
+// escape_pods=2
+
+//GroupTree represents tree of groups, subgroups and end nodes (hosts) with variables
+type GroupTree struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Date        string `json:"date"`
+}
+
+//MasterGroup defines master group (All)
+type MasterGroup struct {
+	Grouped   GroupTree
+	Ungrouped GroupTree
+}
+
 //xProcessInput process inventories
 func xProcessInput(inp string) {
 	// Split on comma.
